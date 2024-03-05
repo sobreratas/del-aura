d00a4a99d3c091076b3ec6c0ef613399fcaddbc6
sobreratas <111362320+sobreratas@users.noreply.github.com>
2024-03-05 15:06:15 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,86 @@
+//FADE IN - LEFT
+const fadeIn = document.querySelectorAll(".fade-in");
+
+const fadeInObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show"); // Add show class when intersected
+        fadeInObserver.unobserve(entry.target); // Stop observing once shown
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+fadeIn.forEach((fade) => {
+  fadeInObserver.observe(fade);
+});
+
+//FADE IN 2 - MIDDLE
+const fadeIn2 = document.querySelectorAll(".fade-in-2");
+
+const fadeIn2Observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show"); // Add show class when intersected
+        fadeIn2Observer.unobserve(entry.target); // Stop observing once shown
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+fadeIn2.forEach((fade2) => {
+  fadeIn2Observer.observe(fade2);
+});
+
+//FADE IN 3 - MIDDLE
+const fadeIn3 = document.querySelectorAll(".fade-in-3");
+
+const fadeIn3Observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show"); // Add show class when intersected
+        fadeIn3Observer.unobserve(entry.target); // Stop observing once shown
+      }
+    });
+  },
+  {
+    threshold: 0.5,
+  }
+);
+
+fadeIn3.forEach((fade3) => {
+  fadeIn3Observer.observe(fade3);
+});
+
+//RELEASE CLASS INTERSECTION OBSERVER 
+const releases = document.querySelectorAll(".release");
+
+const releasesObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+        if (entry.isIntersecting) releasesObserver.unobserve(entry.target)
+    })
+},
+    {
+        threshold: .5,
+    })
+
+releases.forEach(release => {
+    releasesObserver.observe(release)
+})
+
+//FOOTER COPYRIGHT TEXT
+const year = document.querySelector('.year');
+const currentYear = new Date().getFullYear();
+
+
+year.innerHTML = currentYear;
